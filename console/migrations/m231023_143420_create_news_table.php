62b8b2bb8678bf11527ab9272837dabe2f99d519
Murad <M.Schumacher85@mail.ru>
2023-10-23 17:59:21 +0300

    Соэдание БД

---
+++ console/migrations/m231023_143420_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news}}`.
+ */
+class m231023_143420_create_news_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%news}}', [
+            'id' => $this->primaryKey(),
+            'slug' => $this->string(256)->notNull()->unique(),
+            'title' => $this->string(256)->notNull(),
+            'description' => $this->text()->notNull(),
+
+            'enabled' => $this->boolean()->notNull()->defaultValue(0),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%news}}');
+    }
+}
