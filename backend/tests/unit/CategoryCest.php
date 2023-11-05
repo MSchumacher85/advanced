cef10ce71f96fdefb036887780eea683d263fa26
Murad <M.Schumacher85@mail.ru>
2023-11-05 22:00:08 +0300

    тестирование

---
+++ backend/tests/unit/CategoryCest.php
@@ -0,0 +1,14 @@
+<?php
+namespace backend\tests\unit;
+use backend\tests\UnitTester;
+class CategoryCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
